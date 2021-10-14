b10db92050fa4f1615f4b23246b1a8d87cda92e3
Marc Girona-Mata <55145585+mgironamata@users.noreply.github.com>
2021-10-14 19:08:04 +0100

    Update hydrological-analysis.py

--- hydrological-analysis.py
+++ hydrological-analysis.py
@@ -13,33 +13,44 @@ create_geodatabase(gdb_folder_path, gdb_name)
 # Define filename root
 out_filename_root = 'DTM'
 
-# Calculate fill 
-outFill = Fill(inSurfaceRaster, zLimit)
-
-# Calculate flow direction
-outFlowDirection = FlowDirection(outFill, "NORMAL")
-
-# Calculate flow acccumulation
-outFlowAccumulation = FlowAccumulation(outFlowDirection)
-
 # Set flow accumulation threshold
 flow_acc_threshold = 100000
 
-# Calculate flow acccumulation
-outFlowAccumulationSetNull = SetNull(outFlowAccumulation < flow_acc_threshold, 1)
+def hydrological_routing(gdb_folder_path, gdb_name, out_filename_root, flow_acc_threshold=1000000):
+    """ Hydrological routing routine, which generates the following:
+        - Fill raster
+        - Flow direction raster
+        - Flow accumulation raster
+        - Flow network polygons
+        - Fill zones polygons
+    """
+    
+    # Calculate fill 
+    outFill = Fill(inSurfaceRaster, zLimit)
 
-# Save rasters
-outFill.save(gdb_folder_path, gdb_name, f'{out_filename_root}_fill')
-outFlowDirection.save(gdb_folder_path, gdb_name, f'{out_filename_root}_fdir')
-outFlowAccumulation.save(gdb_folder_path, gdb_name, f'{out_filename_root}_facc')
+    # Calculate flow direction
+    outFlowDirection = FlowDirection(outFill, "NORMAL")
 
-# Calculate Fill - DTM Difference
-outFillDiff = SetNull((outFill - inSurfaceRaster) < 0, 1)
+    # Calculate flow acccumulation
+    outFlowAccumulation = FlowAccumulation(outFlowDirection)
 
-# Convert Fill Difference raster to polygon and export
-outPolygons_filename(f'{filename_root}_Fill_polygons')
-outPolygons = os.path.join(gdb_folder_path,gdb_name,outPolygons_filename)
-arcpy.RasterToPolygon_conversion(outFillDiff, outPolygons, "NO_SIMPLIFY")
+    # Calculate flow network (raster)
+    outFlowAccumulationNetwork = SetNull(outFlowAccumulation < flow_acc_threshold, 1)
 
+    # Save rasters
+    outFill.save(gdb_folder_path, gdb_name, f'{out_filename_root}_fill')
+    outFlowDirection.save(gdb_folder_path, gdb_name, f'{out_filename_root}_fdir')
+    outFlowAccumulation.save(gdb_folder_path, gdb_name, f'{out_filename_root}_facc')
 
+    # Calculate Fill - DTM Difference
+    outFillDiff = SetNull((outFill - inSurfaceRaster) < 0, 1)
 
+    # Convert Fill Difference raster to polygon and export
+    outPolygons_filename(f'{filename_root}_Fill_polygons')
+    outPolygons = os.path.join(gdb_folder_path,gdb_name,outPolygons_filename)
+    arcpy.RasterToPolygon_conversion(outFillDiff, outPolygons, "NO_SIMPLIFY")
+
+    # Convert flow network raster to polygon and export
+    outPolygons_filename(f'{filename_root}_flow_network_polygons')
+    outPolygons = os.path.join(gdb_folder_path,gdb_name,outPolygons_filename)
+    arcpy.RasterToPolygon_conversion(outFlowAccumulationNetwork, outPolygons, "NO_SIMPLIFY")
