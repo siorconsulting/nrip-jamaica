29192b679db3807d90ec276b36fafae0f6906d29
Marc Girona-Mata <55145585+mgironamata@users.noreply.github.com>
2021-10-14 08:40:24 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,59 @@
+import arcpy
 
+__all__ = ['change_first_character_to_alphabetic',
+           'rename_feature_class_if_already_exists',
+           'create_geodatabase'
+          ]
+
+def change_first_character_to_alphabetic(name):
+    """Adds AA_ prefix to a string if its first character is not alphabetic
+    
+    Inputs:
+        name : string
+        
+    Returns:
+        name : string
+    """
+    if not(name[0].isalpha()):
+        return 'AA_' + name
+    else:
+        return name
+
+def rename_feature_class_if_already_exists(gdb_folder_path, gdb_name, out_feature_class):
+    """Adds numeric suffix to a feature class name if this already exists in the specified geodatabase
+    
+    Inputs:
+        gdb_folder_path : str
+        gdb_name : str
+        out_feature_class : str
+        
+    Returns:
+        out_feature_clas : str
+    """
+    if not(arcpy.Exists(os.path.join(gdb_folder_path,gdb_name,out_feature_class))):
+        return out_feature_class
+    
+    else:
+        new_out_feature_class = out_feature_class + '_1'
+    
+        i = 1
+        while arcpy.Exists(os.path.join(gdb_folder_path,gdb_name,new_out_feature_class)):
+            i = i + 1
+            new_out_feature_class = out_feature_class + f'_{i}'
+    
+        return new_out_feature_class
+    
+
+def create_geodatabase(gdb_folder_path, gdb_name):
+    """Creates a new file geodabatase at the specified location and, if needed, overwrites the existing one.
+    
+    Inputs:
+        gdb_folder_path : str
+        gdb_name : str
+        
+    Returns:
+        None
+        
+    """
+    arcpy.management.Delete(os.path.join(gdb_folder_path,gdb_name))
+    arcpy.management.CreateFileGDB(gdb_folder_path, gdb_name)
