0c4af13160aa29800025d86216962e5d417b1246
Marc Girona-Mata <55145585+mgironamata@users.noreply.github.com>
2021-10-14 19:55:25 +0100

    Update inundation-analysis.py

--- inundation-analysis.py
+++ inundation-analysis.py
@@ -1,1 +1,21 @@
+import arcpy
+from arcpy import env
+from arcpy.sa import *
+
+def set_null_between(inRaster, low_th, high_th):
+  """ Creates constant valued raster based on two SetNull operations.
+  
+  Input:
+      inRaster : raster
+      low_th : float
+      high_th : float
+  
+  Return:
+      betweenRaster : raster
+  
+  """
+  
+  lowRaster = SetNull(inRaster > high_th, inRaster)
+  betweenRaster = SetNull(lowRaster < low_th, 1)
+  return betweenRaster
 
