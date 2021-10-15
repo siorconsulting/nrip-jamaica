40b9169963d877f5c1c28f08fa19e8bdc7416d1e
Marc Girona-Mata <55145585+mgironamata@users.noreply.github.com>
2021-10-15 15:29:20 +0100

    Update geometric-analysis.py

--- geometric-analysis.py
+++ geometric-analysis.py
@@ -3,8 +3,19 @@ from arcpy import env
 from arcpy.sa import *
 
 
-def calculate_proximity():
-    """Calculates proximity based on Euclidean distance"""
+def calculate_proximity(inSourceData, maxDistance, cellSize, outDirectionRaster):
+    """Calculates proximity based on Euclidean distance
+    
+    Inputs:
+        inSourceData : raster
+        maxDistance : float
+        cellSize : float
+        outDirectionRaster : raster
+        
+    Return:
+        outEucDistance : raster
+        
+    """
     outEucDistance = EucDistance(inSourceData, maxDistance, cellSize, outDirectionRaster)
     return outEucDistance
 
