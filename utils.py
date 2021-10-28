8b1129a40790f60275b557c0847f7ae50a87bd23
Nishant Deswal <92514912+nishantdeswal@users.noreply.github.com>
2021-10-28 18:24:03 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ from arcpy.sa import *
 __all__ = ['change_first_character_to_alphabetic',
            'rename_feature_class_if_already_exists',
            'create_geodatabase',
-           'reclassify_raster', # NOT TESTED
+           'reclassify_raster',
            'set_symbology_from_layer', # NOT TESTED
           ]
 
@@ -65,13 +65,14 @@ def create_geodatabase(gdb_folder_path, gdb_name):
     return print("geodatabase successfully created")
 
 
-def reclassify_raster(in_raster, reclass_field, save_raster=False, save_raster_path=None):
+def reclassify_raster(in_raster, reclass_field="Value", save_raster=False, save_raster_path=None):
     """Reclassify raster values.
     
     Inputs:
            in_raster : raster
-           reclass_field :
-           remap :
+           reclass_field : 
+           save_raster :
+           save_raster_path : 
     
     Returns:
            None
@@ -81,16 +82,16 @@ def reclassify_raster(in_raster, reclass_field, save_raster=False, save_raster_p
 
     p = [np.percentile(array, q) for q in [0,20,40,60,80,100]]
     
-    outReclass = Reclassify(in_raster, "Value", 
+    outReclass = Reclassify(in_raster, reclass_field, 
                             RemapRange([[p[0],p[1],1],
                                         [p[1],p[2],2],
                                         [p[2],p[3],3],
                                         [p[3],p[4],4],
                                         [p[4],p[5],5],
-                                       ])
-
+                                       ]))
+                           
     if save_raster:
-           outReclass.save(save_raster_path)
+        outReclass.save(save_raster_path)
     
     return outReclass
 
