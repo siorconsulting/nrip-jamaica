44b2ac7cae98a835dbadf77a337ca19bdef79993
joelydon14@gmail.com <you@example.com>
2021-11-04 20:15:21 +0000

    Changes to documentation and tools

--- wbt_geometric_analysis.py
+++ wbt_geometric_analysis.py
@@ -5,7 +5,7 @@ import geopandas as gpd
 # import whitebox
 # wbt = whitebox.WhiteboxTools()
 
-wbt = wbt_setup()
+wbt = wbt_setup() 
 
 __all__ = ['intersect', # NOT TESTED
            'zonal_statistics', # NOT TESTED
@@ -78,10 +78,22 @@ def distance_from_points(input_points, output_raster):
     Returns:
         None
     """
+
+    temp_input_points = 'temp_input_points.shp'
+
+    gdf = gpd.read_file(input_points)
+    gdf['FID_wbt'] = 1
+    gdf.to_file(temp_input_points)
+
     input_raster = 'temp_input_raster.tif' # assigning file name
-    wbt.vector_points_to_raster(input_points,input_raster) # points to raster transformation
-    wbt.euclidean_distance(input_raster, output_raster) # euclidean distance calculated on created raster
-    os.remove(os.path.join(wbt.work_dir,input_raster)) # removes temporary raster file
+    input_raster_zeros = 'temp_input_raster_zeros.tif'
+    wbt.vector_points_to_raster(temp_input_points,input_raster,field='FID_wbt', cell_size=100) # points to raster transformation
+    wbt.convert_nodata_to_zero(input_raster, input_raster_zeros)
+    wbt.euclidean_distance(input_raster_zeros, output_raster) # euclidean distance calculated on created raster
+    os.remove(os.path.join(wbt.work_dir, input_raster)) # removes temporary raster file
+    os.remove(os.path.join(wbt.work_dir, input_raster_zeros))
+    os.remove(os.path.join(wbt.work_dir, temp_input_points))
+    os.chdir(wbt.work_dir)
 
 def distance_from_lines(input_lines, output_raster):
     """
@@ -90,37 +102,36 @@ def distance_from_lines(input_lines, output_raster):
     Inputs: 
         input_lines: str <-- path to input point(.shp) file
         output_raster: str <-- raster(.tif) file name
+
+    Outputs:
+        output_raster: raster <-- raster(.tif) file
+
+    Returns:
+        None
     """
+    temp_input_lines = 'temp_input_lines.shp'
+
+    gdf = gpd.read_file(input_lines)
+    gdf['FID_wbt'] = 1
+    gdf.to_file(temp_input_lines)
+
     input_raster = 'temp_input_raster.tif'  # assigning file name
-    wbt.vector_lines_to_raster(input_lines,input_raster) # lines to raster transformation
-    wbt.euclidean_distance(input_raster, output_raster) # euclidean distance calculated on created raster
+    temp_input_raster_zeros = 'temp_input_raster_zeros.tif'
+    wbt.vector_lines_to_raster(temp_input_lines, input_raster,field='FID_wbt', cell_size=100) # lines to raster transformation
+    wbt.convert_nodata_to_zero(input_raster,temp_input_raster_zeros)
+    wbt.euclidean_distance(temp_input_raster_zeros, output_raster) # euclidean distance calculated on created raster
     os.remove(os.path.join(wbt.work_dir,input_raster)) # removes temporary raster file
+    os.remove(os.path.join(wbt.work_dir,temp_input_lines))
+    os.remove(os.path.join(wbt.work_dir,temp_input_raster_zeros))
+    os.chdir(wbt.work_dir)
 
 def distance_from_polygons(input_polygons, output_raster):
     """
     Creates new raster showing distances between polygons
-    """
-    input_raster = 'temp_input_raster.tif' # assigning file name
-    wbt.vector_polygons_to_raster(input_polygons,input_raster) # polygons to raster transformation
-    wbt.euclidean_distance(input_raster, output_raster) # euclidean distance calculated on created raster
-    os.remove(os.path.join(wbt.work_dir,input_raster)) # removes temporary raster file
-
-def distance_from_raster(input_raster, output_raster):
-    """
-    Creates new raster showing distances within raster
-    """
-    wbt.euclidean_distance(input_raster, output_raster) # euclidean distance calculated on input raster
-
-def hotspots_from_points(input_points, output_raster, filterx=11, filtery=11, k=5):
-    """
-    Creates hotspot raster from input point shapefile
 
     Inputs:
-        input_points: str <-- path to input point(.shp) file
-        output_raster: str <-- name of output raster(.tif) file
-        filterx [optional] : int <-- size of kernel in x-direction, default value is '11'
-        filtery [optional] : int <-- size of kernel in y-direction, default value is '11'
-        k [optional] : int <-- number of nearest-valued neighbours to use, default value is '5'
+        input_polygons: str <-- path to input polygon(.shp) file
+        output_raster: str <-- raster(.tif) file name
 
     Outputs:
         output_raster: raster <-- output raster(.tif) file
@@ -128,21 +139,77 @@ def hotspots_from_points(input_points, output_raster, filterx=11, filtery=11, k=
     Returns:
         None
     """
-    input_raster = 'temp_hotspots_from_points.tif' # assigning temporary file name
-    wbt.vector_points_to_raster(input_points, input_raster) # points to raster transformation
-    wbt.k_nearest_mean_filter(input_raster, output_raster, filterx=filterx, filtery=filtery, k=k) 
-    os.remove(os.path.join(wbt.work_dir, input_raster)) # remove temporary raster file
+    temp_input_polygons = 'temp_input_polygons.shp'
+    temp_input_raster_zeros = 'temp_input_raster_zeros.tif'
+    gdf = gpd.read_file(input_polygons)
+    gdf['FID_wbt'] = 1
+    gdf.to_file(temp_input_polygons)
 
-def hotspots_from_lines(input_lines, output_raster, filterx=11, filtery=11, k=5):
+    input_raster = 'temp_input_raster.tif' # assigning file name
+    wbt.vector_polygons_to_raster(temp_input_polygons ,input_raster, field='FID_wbt', cell_size = 100) # polygons to raster transformation
+    wbt.convert_nodata_to_zero(input_raster, temp_input_raster_zeros)
+    wbt.euclidean_distance(temp_input_raster_zeros, output_raster) # euclidean distance calculated on created raster
+    os.remove(os.path.join(wbt.work_dir,input_raster)) # removes temporary raster file
+    os.remove(os.path.join(wbt.work_dir,temp_input_polygons))
+    os.remove(os.path.join(wbt.work_dir,temp_input_raster_zeros))
+
+
+def distance_from_raster(input_raster, output_raster):
+    """
+    Creates new raster showing distances within raster
+
+    Inputs:
+        input_raster: str <-- path to input raster(.tif) file
+        output_raster: str <-- raster(.tif) file name
+
+    Outputs:
+        output_raster: raster <-- output raster(.tif) file 
+    """
+    temp_input_raster_zeros = 'temp_input_raster_zeros.tif'
+    wbt.convert_nodata_to_zero(input_raster, temp_input_raster_zeros)
+    wbt.euclidean_distance(temp_input_raster_zeros, output_raster) # euclidean distance calculated on input raster
+    os.remove(os.path.join(wbt.work_dir, temp_input_raster_zeros))
+
+def hotspots_from_points(input_points, output_raster, sigma = 100):
+    """
+    Creates hotspot raster from input point shapefile
+
+    Inputs:
+        input_points: str <-- path to input point(.shp) file
+        output_raster: str <-- name of output raster(.tif) file
+        sigma [optional]: str <-- represents standard deviation of gaussian filter, default 100
+
+    Outputs:
+        output_raster: raster <-- output raster(.tif) file
+
+    Returns:
+        None
+    """
+    temp_input_points = 'temp_input_points.shp'
+
+    gdf = gpd.read_file(input_points)
+    gdf['FID_wbt'] = 1
+    gdf.to_file(temp_input_points)
+
+    
+    input_raster = 'temp_hotspots_from_points.tif' # assigning temporary file name
+    temp_input_raster_zeros = 'temp_input_raster_zeros.tif'
+
+    wbt.vector_points_to_raster(temp_input_points, input_raster, field='FID_wbt', cell_size=100) # points to raster transformation
+    wbt.convert_nodata_to_zero(input_raster, temp_input_raster_zeros)
+    wbt.gaussian_filter(temp_input_raster_zeros, output_raster, sigma=sigma)
+    os.remove(os.path.join(wbt.work_dir, input_raster)) # remove temporary raster file
+    os.remove(os.path.join(wbt.work_dir, temp_input_raster_zeros))
+    os.remove(os.path.join(wbt.work_dir, temp_input_points))
+
+def hotspots_from_lines(input_lines, output_raster, sigma =10):
     """
     Creates hotspot raster from input line shapefile
 
     Inputs:
         input lines: str <-- path to input line(.shp) file
         output_raster: str <-- raster(.tif) file name
-        filterx [optional] : int <-- size of kernel in x-direction, default value is '11'
-        filtery [optional] : int <-- size of kernel in y-direction, default value is '11'
-        k [optional] : int <-- number of nearest-valued neighbours to use, default value is '5'
+        sigma [optional]: str <-- represents standard deviation of gaussian filter, default 10
 
     Outputs:
         output_raster: raster <-- raster(.tif) file 
@@ -150,21 +217,31 @@ def hotspots_from_lines(input_lines, output_raster, filterx=11, filtery=11, k=5)
     Returns:
         None
     """
-    input_raster = 'temp_hotspots_from_lines.tif' # assigning temporary file name
-    wbt.vector_lines_to_raster(input_lines, input_raster) # lines to raster transformation
-    wbt.k_nearest_mean_filter(input_raster, output_raster, filterx=filterx, filtery=filtery, k=k)
-    os.remove(os.path.join(wbt.work_dir, input_raster)) # remove temporary file
+    temp_input_lines = 'temp_input_lines.shp'
 
-def hotspots_from_polygons(input_polygons, output_raster, filterx=11, filtery=11, k=5):
+    gdf = gpd.read_file(input_lines)
+    gdf['FID_wbt'] = 1
+    gdf.to_file(temp_input_lines)
+
+    input_raster = 'temp_hotspots_from_lines.tif' # assigning temporary file name
+    temp_input_raster_zeros = 'temp_input_raster_zeros.tif'
+
+    wbt.vector_lines_to_raster(temp_input_lines, input_raster, field = 'FID_wbt', cell_size = 100) # lines to raster transformation
+    wbt.convert_nodata_to_zero(input_raster, temp_input_raster_zeros)
+    wbt.gaussian_filter(temp_input_raster_zeros, output_raster, sigma=sigma)
+    os.remove(os.path.join(wbt.work_dir, input_raster)) # remove temporary raster file
+    os.remove(os.path.join(wbt.work_dir, temp_input_raster_zeros))
+    os.remove(os.path.join(wbt.work_dir, temp_input_lines)) # remove temporary file
+
+
+def hotspots_from_polygons(input_polygons, output_raster,sigma = 10):
     """
     Creates hotspot raster from input polygon shapefile
 
     Inputs:
         input polygons: str <-- path to input line(.shp) file
         output_raster: str <-- raster(.tif) file name
-        filterx [optional] : int <-- size of kernel in x-direction, default value is '11'
-        filtery [optional] : int <-- size of kernel in y-direction, default value is '11'
-        k [optional] : int <-- number of nearest-valued neighbours to use, default value is '5'
+        sigma [optional]: str <-- represents standard deviation of gaussian filter, default 10
 
     Output:
         output_raster: raster <-- raster(.tif) file
@@ -172,21 +249,30 @@ def hotspots_from_polygons(input_polygons, output_raster, filterx=11, filtery=11
     Returns:
     None
     """
-    input_raster = 'temp_hotspots_from_polygons.tif' # assigning temporary file name
-    wbt.vector_polygons_to_raster(input_polygons, input_raster) # polygons to raster transformation
-    wbt.k_nearest_mean_filter(input_raster, output_raster, filterx=filterx, filtery=filtery, k=k)
-    os.remove(os.path.join(wbt.work_dir, input_raster)) # remove temporary file
+    temp_input_polygons = 'temp_input_polygons.shp'
 
-def hotspots_from_raster(input_raster, output_raster, filterx=11, filtery=11, k=5):
+    gdf = gpd.read_file(input_polygons)
+    gdf['FID_wbt'] = 1
+    gdf.to_file(temp_input_polygons)
+
+    input_raster = 'temp_hotspots_from_polygons.tif' # assigning temporary file name
+    temp_input_raster_zeros = 'temp_input_rasters_zeros.tif'
+    wbt.vector_polygons_to_raster(temp_input_polygons, input_raster, field='FID_wbt', cell_size=100) # polygons to raster transformation
+    wbt.convert_nodata_to_zero(input_raster, temp_input_raster_zeros)
+    wbt.gaussian_filter(temp_input_raster_zeros, output_raster, sigma=sigma)
+    os.remove(os.path.join(wbt.work_dir, input_raster)) # remove temporary file
+    os.remove(os.path.join(wbt.work_dir, temp_input_polygons)) 
+    os.remove(os.path.join(wbt.work_dir, temp_input_raster_zeros))
+
+
+def hotspots_from_raster(input_raster, output_raster,sigma = 10):
     """
     Creates hotspot raster from input raster file
 
     Inputs:
-        input polygons: str <-- path to input line(.shp) file
+        input raster: str <-- path to input raster(.tif) file
         output_raster: str <-- raster(.tif) file name
-        filterx [optional] : int <-- size of kernel in x-direction, default value is '11'
-        filtery [optional] : int <-- size of kernel in y-direction, default value is '11'
-        k [optional] : int <-- number of nearest-valued neighbours to use, default value is '5'
+        sigma [optional]: str <-- represents standard deviation of gaussian filter, default 10
     
     Outputs:
         output_raster: raster <-- raster(.tif) file
@@ -194,9 +280,12 @@ def hotspots_from_raster(input_raster, output_raster, filterx=11, filtery=11, k=
     Returns:
         None
     """
-    wbt.k_nearest_mean_filter(input_raster, output_raster, filterx=filterx, filtery=filtery, k=k)
+    temp_input_raster_zeros = 'temp_input_raster_zeros.tif'
 
-def interpolate_points(input_points, output_raster):
+    wbt.convert_nodata_to_zero(input_raster, temp_input_raster_zeros)
+    wbt.gaussian_filter(temp_input_raster_zeros, output_raster, sigma=sigma)
+
+def interpolate_points(input_points, field, output_raster):
     """
     Intepolates points into a raster surface 
 
@@ -210,7 +299,7 @@ def interpolate_points(input_points, output_raster):
     Returns:
         None
     """
-    wbt.radial_basis_function_interpolation(i=input_points, output=output_raster) # interpolation function
+    wbt.radial_basis_function_interpolation(field, i=input_points, output=output_raster) # interpolation function
 
 def SummarizeWithin(input_vector, feature_polygons, output_polygon, field_to_summarize, aggfunc='mean'):
     """
@@ -225,6 +314,9 @@ def SummarizeWithin(input_vector, feature_polygons, output_polygon, field_to_sum
         
     Outputs:
         output_polygon: shapefile <-- polygon(.shp) file
+
+    Returns:
+        None
     """
     input_vector = gpd.read_file(input_vector) # geopandas read vector
     feature_polygons = gpd.read_file(feature_polygons) # geopandas polygons 
